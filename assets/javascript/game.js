2b64fe9b94655d540012727f6c548487f192861c
Tina Nguyen <nguyentina2005@yahoo.com>
2017-05-20 21:26:14 -0400

    button add to score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,23 +25,24 @@ var totalValue = 0;
 var win = 0;
 var lose = 0;
 //random numbers for buttons
-var bt1 = Math.floor(Math.random()*12 + 1);
-var bt2 = Math.floor(Math.random()*12 + 1);
-var bt3 = Math.floor(Math.random()*12 + 1);
-var bt4 = Math.floor(Math.random()*12 + 1);
+var bt1 = Math.floor(Math.random()*11 + 1);
+var bt2 = Math.floor(Math.random()*11 + 1);
+var bt3 = Math.floor(Math.random()*11 + 1);
+var bt4 = Math.floor(Math.random()*11 + 1);
 
 function Start(){
 //generating a random number to reach
-	var RandomnNumber = Math.floor(Math.random()*102 + 19);
+	var RandomnNumber = Math.floor(Math.random()*101 + 19);
 //print random number in html
 	$("#randomNumber").html(RandomnNumber);
 //debuging (make sure the right number is appearing)
 	console.log("The number is " +RandomnNumber);
 //random number for button 1
-	bt1 = Math.floor(Math.random()*12 + 1);
-	bt2 = Math.floor(Math.random()*12 + 1);
-	bt3 = Math.floor(Math.random()*12 + 1);
-	bt4 = Math.floor(Math.random()*12 + 1);
+	bt1 = Math.floor(Math.random()*11 + 1);
+	bt2 = Math.floor(Math.random()*11 + 1);
+	bt3 = Math.floor(Math.random()*11 + 1);
+	bt4 = Math.floor(Math.random()*11 + 1);
+	
 //making sure there are 4 random numbers	
 	console.log("button ones is "+ bt1);
 	console.log("button two is " + bt2);
@@ -49,24 +50,75 @@ function Start(){
 	console.log("button four is " + bt4);
 //players current total number	
 	totalValue = 0;
-	$("#totalVaule").html(totalValue);
-	console.log(totalValue);
+	$("#CurrentValue").html(totalValue);
+	console.log("Starting Value " + totalValue);
 }
  //testing random given number function (It works)
 
-      $("#bt1").on("click", function() {
-        alert("One been clicked!");
+
+//first button
+    $("#bt1").on("click", function() {
+//the addition
+       totalValue = bt1 + totalValue;
+//post the new added number to html
+       $("#CurrentValue").html(totalValue);
+//To see if the right number is added correcetly and its new value is correctly added.
+       console.log("New total " + totalValue);
       });
-        $("#bt2").on("click", function() {
-        alert("Two been clicked!");
+
+//Second button    
+    $("#bt2").on("click", function() {
+    	totalValue = bt2 + totalValue;
+    	$("#CurrentValue").html(totalValue);
+    	console.log("New total " + totalValue);
       });
-          $("#bt3").on("click", function() {
-        alert("Three been clicked!");
+
+//Third button
+    $("#bt3").on("click", function() {
+    	totalValue = bt3 + totalValue;
+    	$("#CurrentValue").html(totalValue);
+        console.log("New total " + totalValue);
       });
-            $("#bt4").on("click", function() {
-        alert("Four been clicked!");
+
+//Fourth button
+    $("#bt4").on("click", function() {
+    	totalValue = bt4 + totalValue;
+    	$("#CurrentValue").html(totalValue);
+        console.log("New total " + totalValue);
       });
-//Start();
+
+//adding to the scoreboard to keep track of wins and lost
+function roundComplete() {
+  
+  if (totalValue === RandomnNumber) {
+    win++;
+
+    $("#win-counter").html(win);
+    Start();
+  }
+     
+  
+  else if (totalValue > RandomnNumber) {
+    
+    lose++;
+
+    $("loss-counter").html(lose) ;
+
+    Start();
+  }
+    console.log("wins" + " " + win);
+    console.log("lose" + " " + lose);
+}
 
 
+
+
+
+
+
+
+
+
+
+Start();
 }); 
